10a1d473654ca8044bac5187ed116df5142806f3
Minal Karani <karani.minal@gmail.com>
2019-02-11 23:28:47 -0500

    Bamazon

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table'); 
-var Validation = require("./validationModule.js")
 require("dotenv").config();
 
 
